f75c6be954a0acca8ffbe28399983019f27a6348
Gerry Fan <gerry.fan@gmail.com>
2024-06-13 14:19:56 -0700

    Use TextInput

--- tutorial01/App.js
+++ tutorial01/App.js
@@ -1,40 +1,42 @@
+// TextInput
 import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View, Button, Text } from 'react-native';
+import { StyleSheet,View, Button, Text, TextInput } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState("Gerry");
-  const clickMe = () => {
-    setName("Knowledge");      //update the "name" state.
-    setPerson({name:"Jason", age:24});  //Update the "person" state.
-  };
+  const [age, setAge] = useState(24);
 
-  // example of other cobject state:
-  const [person, setPerson] = useState({name:"Tony", age:23});
 
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
-      <Text>Myname is {person.name} and my age is {person.age}</Text>
-      <View style={styles.button}>
-        <Button title="change" color="yellow" onPress={clickMe} />
-      </View>
+      <Text>Enter your name:</Text>
+      <TextInput style={styles.input}
+                 onChangeText={(text)=>setName(text)}
+                 placeholder="Enter your name"/>
+      <Text>Enter your age:</Text>
+      <TextInput style={styles.input}
+                 onChangeText={(text)=>setAge(text)}
+                 placeholder="Enter your age"
+                 keyboardType='numeric'/>
+
+      <Text>My name is {name} and my age is {age}.</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: "40%",
     flex: 1,
-    color: "blue",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    color:"yellow",
-    backgroundColor:"red",
-    padding: 10
+  input: {
+    borderColor:"black",
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    width: 150,
   }
 });
