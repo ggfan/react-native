9daff7297ad3761502db6b03a1c1b719f2e2f7cb
Gerry Fan <gerry.fan@gmail.com>
2024-06-13 13:27:27 -0700

    Button Usage

--- tutorial01/App.js
+++ tutorial01/App.js
@@ -1,18 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View, Button, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Native Tutoiral!</Text>
-      <StatusBar style="auto" />
+      <Button id={1} title="click me" color="red" onPress={() => alert("Buttone clicked")} disabled={false}></Button>
+      <Button id={2} title="OK" color="black" onPress={() => alert("This is a button Tutorial")} ></Button>
+      <Text>hello</Text>
+      <Text>
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: "40%",
     flex: 1,
+    color: "blue",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
