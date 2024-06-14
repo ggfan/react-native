a2c92a2a0653e1b9e777af140eac9a5027e00342
Gerry Fan <gerry.fan@gmail.com>
2024-06-14 12:03:13 -0700

    use Alert component

--- tutorial01/App.js
+++ tutorial01/App.js
@@ -1,35 +1,31 @@
-//StyleSheet:
-//  1. inline style(s)
-//  2. create a StyleSheet in the same file
-//  3. create a StyleSheet in different file, import it
+// Alert()
 import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View, Button, Text, FlatList, TouchableOpacity} from 'react-native';
+import { StyleSheet,View, TextInput, Button, Alert, Text } from 'react-native';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name: "Gerry",key:"1"},
-    {name: "Jerry", key:"2"},
-    {name: "Jeff", key: "3"},
-    {name: "Tony", key:"4"},
-    {name: "Bruce", key: "5"},
-  ]);
-
-  function pressHandler(str) {
-      console.log("pressHandler for :" + str);
+  const [age, setAge] = useState(24);
+  const ageHandler = () => {
+    if (age > 21)
+    {
+      Alert.alert("Yay");
+    } else {
+      Alert.alert("Nah");
+    }
   }
+
   return (
     <View style={styles.container}>
-        <FlatList
-            data = {people}
-            renderItem = {({item})=> (
-              <View>
-                <TouchableOpacity onPress={()=>pressHandler(item.name)}>
-                  <Text style={styles.item}>{item.name}</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-        />
+      <Text>Your Name:</Text>
+      <TextInput style={styles.input} placeholder="Your name:"/>
+      <Text>Your Age:</Text>
+      <TextInput style={styles.input}  placeholder="Your age" onChangeText={(text)=>(setAge(text))}/>
+      {/*
+        Button Title: on Android (L+) are all capticalized. Need find a way to turn it off.
+                      on Apple: works file.
+        label: does not work on either OS.
+      */}
+      <Button title="Can I Drink?" onPress={()=>ageHandler()}></Button>
     </View>
   );
 }
@@ -37,15 +33,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
+    color: "black",
     marginTop:20,
     padding:20,
 //    alignItems: 'center',
 //    justifyContent: 'center',
   },
-  item: {
-    fontSize: 20,
-    padding: 15,
+  input: {
     backgroundColor: 'yellow',
+    padding: 20,
+    borderWidth:4,
+    marginTop: 10,
+    color: "red",
   }
 });
