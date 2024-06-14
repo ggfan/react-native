ffd165bb24200e7e4ad4b715f3a109333a894881
Gerry Fan <gerry.fan@gmail.com>
2024-06-14 16:23:00 -0700

    dismiss Keyboard

--- tutorial01/App.js
+++ tutorial01/App.js
@@ -1,32 +1,20 @@
-//Yes-No Button
+//Keyboard: dismiss (does not have a function to display it though)
 import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View, Button, TextInput, Alert, Text, Image, Dimensions } from 'react-native';
+import { StyleSheet,View, Button, TextInput, Alert, Text, Image, Dimensions,
+         TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 const prompt = {name: "your name", age: "your age", gender:"your gender"};
 export default function App() {
-  const [name, setName] = useState(" ");
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState(" ");
-  function pressHandler() {
-    Alert.alert("Details", "Are you sure you want to submit ?", [
-      {text:"Yes", onPress:()=>console.log("yes is pressed")},
-      {text:"No", onPress:()=>noHandler()},
-    ])
-  }
-  function noHandler() {
-    setName(prompt.name);
-    setAge(prompt.age);
-    setGender(prompt.gender);
-  }
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.input} placeholder={prompt.name} onChangeText={(text)=>setName(text)}/>
-      <TextInput style={styles.input} placeholder={prompt.age} onChangeText={(text)=>setAge(text)}/>
-      <TextInput style={styles.input} placeholder={prompt.gender} onChangeText={(text)=>setGender(text)}/>
-      <Button title="Submit" onPress={()=>pressHandler()}></Button>
-      <Text>{name}  {age}  {gender}</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.isVisible()? Keyboard.dismiss() : {};
+      console.log("Keyboard is dismissed.");
+    }}>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Your name"/>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -39,7 +27,8 @@ const styles = StyleSheet.create({
     padding:20,
   },
   input: {
-    marginTop:10,
+    marginTop:20,
+    padding: 20,
     backgroundColor: 'pink',
     color: "blue",
   }
